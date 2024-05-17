92ea5848b9a712cc903d2fcca984754494a91ba0
JzCo42 <117298062+JzCo42@users.noreply.github.com>
2024-05-17 12:02:35 +0200

    Update parser.hpp

--- parser_0/inc/parser.hpp
+++ parser_0/inc/parser.hpp
@@ -5,8 +5,8 @@
 #include <iostream>
 #include <fstream>
 #include <list>
-#include "server_struct/ServerStruct.hpp"
-#include "text_file_parser/Parser.hpp"
+#include "../server_struct/ServerStruct.hpp"
+#include "../text_file_parser/Parser.hpp"
 
 //file_handler.cpp
 int	load_file_to_buff(char *file_name, char **buffer, int *file_len);
