bf1d80c5b0e9ded4d5959702ed9fd53cf618f7b2
Martin Haan <mhaan@f1r3s6.codam.nl>
2024-07-04 13:06:17 +0200

    renamed and updated cgi_time.cpp to datetime.cpp

---
+++ cgi_src/datetime.cpp
@@ -0,0 +1,16 @@
+#include "cgi.hpp"
+#include <ctime>
+#include <iostream>
+
+int main() {
+  std::cout << "Content-type: " << "text/html\r\n\r\n";
+  std::cout << "<!DOCTYPE html><html lang=\"en\"><head><title>Datetime</title></head><body>"
+		<< std::endl;
+
+  time_t currentTime;
+  time(&currentTime);
+  std::cout << asctime(localtime(&currentTime));
+
+  std::cout << "</body></html>";
+  return 0;
+}
