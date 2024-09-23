666caf43373e9874bea868e7a0937f37b9e429ed
Joost Zeeuw Van Der Laan <jzeeuw-v@f1r6s2.codam.nl>
2024-09-23 15:34:45 +0200

    delete php-bin

--- html/php-bin/1.php
+++
@@ -1,5 +0,0 @@
-<?php $myVariable = "Some text";?>
-<form method="post" action="2.php">
- <input type="hidden" name="text" value="<?php echo $myVariable; ?>">
- <button type="submit">Submit</button>
-</form>
